d09e74543511c466f89ed0387d685dba1e51ad58
Dempoleon <dempoleon@gmail.com>
2022-11-09 11:49:11 -0800

    created Character model

---
+++ models/Character.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Charcter extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+Project.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.VARCHAR(50),
+            allowNull: false,
+        },
+        alignment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        personality: {
+            type: DataTypes.STRING,
+        },
+        physical_descbription: {
+            type: DataTypes.TEXT,
+        },
+        image: {
+            type: DataTypes.STRING,
+        },
+        backstory: {
+            type: DataTypes.TEXT,
+        },
+        abilities_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'abilities',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'character',
+    }
+)
