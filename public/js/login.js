7500375d241066d6045592d4f23231366df1a324
doldol123 <toruig59@gmail.com>
2022-11-09 18:05:56 -0800

    create signupFormHnadler

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-// Create login handler
+// login handler
 
 const loginFormHandler = async (e) => {
     e.preventDefault();
@@ -10,17 +10,46 @@ const loginFormHandler = async (e) => {
     if(email && password) {
         // send a POST request to the API endpoint
         const response = await fetch('/api/user/login', {
-            method: POST,
+            method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if(response.ok) {
             // if successful, redirect the browser to the homepage
-            document.location.replace('/')
+            document.location.replace('/');
         } else {
             alert(`Failed to login, please try again!`,response.statusText);
         }
     }
 };
 
+// signup handler to create a new user account
+
+const signupFormHandler = async (e) => {
+    e.preventDefault();
+
+    // get user input value from signup form
+    const name = $("#name-signup").val().trim();
+    const email = $("#email-signup").val().trim();
+    const password = $("#password-signup").val().trim();
+
+    if(name && email && password) {
+        // send a POST request to API
+        const response = await fetch('/api/user', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        }) 
+
+        if(response.ok) {
+            // if successful, redirect the browser to homepage
+            document.location.replace('/');
+        } else {
+            alert(`Please enter correct info to sign up!`, response.statusText);
+        }
+    }
+};
+
+$('.login-form').on('submit', loginFormHandler);
+$('.signup-form').on('submit', signupFormHandler);
