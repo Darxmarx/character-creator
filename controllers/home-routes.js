1a7f00f5f0d09d6a24837a68a32ef87ae6aaa373
Darx <mcrivabode@gmail.com>
2022-11-10 14:16:53 -0800

    further work on home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,3 +2,13 @@
 const router = require('express').Router();
 const { User, Character, Abilities } = require('../models');
 const authorizeUser = require ('../utils/auth');
+
+// render user's personal profile, which contains all the user's characters
+router.get('/profile', authorizeUser, async (req, res) => {
+    try {
+        // find logged in user based on active session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            // do not check/
+        })
+    }
+})
