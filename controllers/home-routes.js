0309abc643a488f47a0efffc4f9218f778baf8ef
Darx <mcrivabode@gmail.com>
2022-11-15 13:12:35 -0800

    create new profile.handlebars so that personal list of characters has 'add character' button

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,41 +3,8 @@ const router = require('express').Router();
 const { User, Character } = require('../models');
 const authorizeUser = require('../utils/auth');
 
-// TODO: commented out for now, please continue
-// render user's personal profile, which contains all the user's characters
-// router.get('/profile', authorizeUser, async (req, res) => {
-//     try {
-//         // find logged in user based on active session ID
-//         const userData = await User.findByPk(req.session.user_id, {
-//             // do not check/
-//         })
-//     }
-// })
-
 router.get('/', authorizeUser, async (req, res) => {
     try {
-
-
-        // const userData = await User.findAll({
-        //     include: [
-        //         {
-        //             attributes: ['name'],
-        //         }
-        //     ]
-        // })
-
-        // const users = userData.map((user) => user.get({ plain: true }));
-
-        // console.log(user);
-
-        // res.render('users', {
-        //     users,
-        //     logged_in: req.session.logged_in,
-        // })
-
-        // when the user logs in we want them to be able to see the characters they made
-        // res.render('users')
-
         if (req.session.logged_in) {
             res.redirect('/user_list');
             return;
@@ -46,7 +13,7 @@ router.get('/', authorizeUser, async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 
 router.get('/login', (req, res) => {
@@ -55,8 +22,8 @@ router.get('/login', (req, res) => {
         return;
     }
 
-    res.render('login')
-})
+    res.render('login');
+});
 
 router.get('/signup', (req, res) => {
     if (req.session.logged_in) {
@@ -64,8 +31,8 @@ router.get('/signup', (req, res) => {
         return;
     }
 
-    res.render('login')
-})
+    res.render('login');
+});
 
 router.get('/characters', (req, res) => {
     try {
@@ -73,7 +40,7 @@ router.get('/characters', (req, res) => {
     } catch(err) {
         res.status(400).json(err);
     }
-})
+});
 
 router.get('/user', async (req, res) => {
     try {
@@ -82,33 +49,28 @@ router.get('/user', async (req, res) => {
                 user_id: req.session.user_id
             },
 
-        })
-
+        });
 
         const characterList = userCharacters.map((character) => character.get({ plain: true }));
 
-        res.render('users-characters', {
+        res.render('profile', {
             characterList,
             logged_in: true
-        })
+        });
     } catch(err) {
         // res.redirect('/login');
         res.status(400).json(err);
     }
-
-})
+});
 
 router.get('/user_list', async (req, res) => {
     try {
-
         const userListData = await User.findAll({
-            // attributes: ["id", "name"]
             attributes: { exclude: ['password', 'email'] }
-        })
+        });
 
         console.log(userListData);
 
-        // const userlist = userListData.get({ plain: true });
         const userlist = userListData.map((user) => user.get({ plain: true }));
 
         console.log(typeof userlist + "@@@@@@@@@@@@@@@@@");
@@ -117,12 +79,11 @@ router.get('/user_list', async (req, res) => {
         res.render('useraccount', {
             userlist,
             logged_in: true
-        })
-
+        });
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 router.get('/user/:user_id/characters', async (req, res) => {
     try {
@@ -130,8 +91,7 @@ router.get('/user/:user_id/characters', async (req, res) => {
             where: {
                 user_id: req.params.user_id
             },
-
-        })
+        });
 
 
         const characterList = userCharacters.map((character) => character.get({ plain: true }));
@@ -139,14 +99,12 @@ router.get('/user/:user_id/characters', async (req, res) => {
         res.render('users-characters', {
             characterList,
             logged_in: true
-        })
+        });
 
     } catch (err) {
         res.status(500).json(err);
     }
-})
-
-
+});
 
 // export data for use elsewhere
 module.exports = router;
